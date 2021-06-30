c000188a512c4b16376bafb8e6feada563299723
Skyfrid7 <skyfrid.webdev@gmail.com>
2021-06-30 21:11:50 +0200

    backend v1

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,27 +9,29 @@
      </div>
 </div>
 
-<div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-     <div class="limited-img">
-          <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
+@foreach $(posts as $post)
+     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+          <div class="limited-img">
+               <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
+          </div>
+          <div>
+               <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                    Apprend à écrire en Laravel
+               </h2>
+
+               <span class="text-gray-500">
+                    Par <span class="font-bold italic text-gray-800">DesiWeb</span>, il y a 1 jour
+               </span>
+
+               <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt nemo omnis eaque corporis fugit perspiciatis nulla velit molestias officiis. Nisi facere voluptatibus itaque tenetur vel ipsum optio dolor architecto adipisci.
+               </p>
+
+               <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                    Continuer à lire
+               </a>
+          </div>
      </div>
-     <div>
-          <h2 class="text-gray-700 font-bold text-5xl pb-4">
-               Apprend à écrire en Laravel
-          </h2>
-
-          <span class="text-gray-500">
-               Par <span class="font-bold italic text-gray-800">DesiWeb</span>, il y a 1 jour
-          </span>
-
-          <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt nemo omnis eaque corporis fugit perspiciatis nulla velit molestias officiis. Nisi facere voluptatibus itaque tenetur vel ipsum optio dolor architecto adipisci.
-          </p>
-
-          <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-               Continuer à lire
-          </a>
-     </div>
-</div>
+@endforeach
 
 @endsection
