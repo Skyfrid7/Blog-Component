9664379227aa7d5782b0eff5e1d9c04fada2c642
Skyfrid7 <skyfrid.webdev@gmail.com>
2021-07-01 19:06:36 +0200

    finished blog component

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +20,7 @@ class PostsController extends Controller
     public function index()
     {
         return view('blog.index')
-            ->with('posts', Post::orderBy('updated_at', 'DESC')->get());
+            ->with('posts', Post::with('user')->orderBy('updated_at', 'DESC')->get()); //with('user') à voir si ça permet de pas faire 1 requete par post ! (commentaire sous la vidéo)
     }
 
     /**
@@ -25,7 +30,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('blog.create');
     }
 
     /**
@@ -36,51 +41,90 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|mimes:jpg,png,jpeg|max:5048'
+        ]);
+
+        $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
+
+        $newImageName = uniqid() . '-' . $slug . '.' . $request->image->extension(); //pose problème selon le titre !
+
+        $request->image->move(public_path('images'), $newImageName);
+
+        Post::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'slug' => $slug,
+            'image_path' => $newImageName,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return redirect('/blog')->with('message', "L'article a été publié !");
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  string $slug
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        return view('blog.show')
+            ->with('post', Post::where('slug', $slug)->first());
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($slug)
     {
-        //
+        return view('blog.edit')
+            ->with('post', Post::where('slug', $slug)->first());
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $slug)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Post::where('slug', $slug)
+            ->update([
+                'title' => $request->input('title'),
+                'description' => $request->input('description'),
+                'slug' => $slug
+            ]);
+
+        return redirect('/blog')
+            ->with('message', "L'article a été mis à jour !");
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($slug)
     {
-        //
+        $post = Post::where('slug', $slug);
+        $post->delete();
+
+        return redirect('/blog')
+            ->with('message', "L'article a été supprimé.");
     }
 }
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,27 +9,62 @@
      </div>
 </div>
 
-@foreach $(posts as $post)
+@if (session()->has('message'))
+    <div class="w-4/5 m-auto mt-10 pl-2">
+          <p class="w-2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl py-4 px-8">
+               {{ session()->get('message') }}
+          </p>
+    </div>
+@endif
+
+@if (Auth::check())
+    <div class="pt-15 w-4/5 m-auto">
+          <a href="/blog/create" class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+               Créer un article
+          </a>
+    </div>
+@endif
+
+@foreach ($posts as $post)
      <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
           <div class="limited-img">
-               <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
+               <img src="{{ asset('images/' . $post->image_path) }}" alt="{{ $post->title }}">
           </div>
           <div>
                <h2 class="text-gray-700 font-bold text-5xl pb-4">
-                    Apprend à écrire en Laravel
+                    {{ $post->title }}
                </h2>
 
                <span class="text-gray-500">
-                    Par <span class="font-bold italic text-gray-800">DesiWeb</span>, il y a 1 jour
+                    Par <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Créé le {{ date('jS M Y', strtotime($post->updated_at)) }}
                </span>
 
                <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt nemo omnis eaque corporis fugit perspiciatis nulla velit molestias officiis. Nisi facere voluptatibus itaque tenetur vel ipsum optio dolor architecto adipisci.
+                    {{ $post->description }}
                </p>
 
-               <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+               <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
                     Continuer à lire
                </a>
+
+               @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
+                   <span class="float-right">
+                        <a href="/blog/{{ $post->slug }}/edit" class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
+                             Editer
+                        </a>
+                   </span>
+
+                   <span class="float-right">
+                         <form action="/blog/{{ $post->slug }}" method="POST">
+                              @csrf
+                              @method('delete')
+
+                              <button type="submit" class="text-red-500 pr-3">
+                                   Supprimer
+                              </button>
+                         </form>
+                   </span>
+               @endif
           </div>
      </div>
 @endforeach
